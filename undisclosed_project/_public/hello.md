3926a9ce08a2b556c004adc439fe8d44716c4895
SODADUHKING <161243450+sodaduhking2@users.noreply.github.com>
2025-03-27 07:40:53 +0000

    Delete undisclosed_project directory

--- undisclosed_project/_public/hello.md
+++
@@ -1,1 +0,0 @@
-hello welcome to scuffedbase beta this website is still under construction and credits to pages-pastebin for making the original pastebin on pages project
--- undisclosed_project/snippet.js
+++
@@ -1,9 +0,0 @@
-ace.config.set('basePath', 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/')
-
-ace.edit('editor', {
-  mode: document.getElementById('editor').attributes.mode.value,
-  selectionStyle: "text",
-  theme: 'ace/theme/chrome',
-  readOnly: true,
-  useWorker: false
-});
